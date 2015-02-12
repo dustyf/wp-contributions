a9b1ab55f128a6ca95822f9b2751d27ed049e3cc
Dustin Filippini <dusty@dustyf.com>
2015-02-12 15:05:56 -0600

    Add plugin starter.

---
+++ wp-contributions.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Plugin Name: WP Contributions
+ * Plugin URI: http://dustyf.com
+ * Description: Show off your WordPress contributions.
+ * Author: Dustin Filippini
+ * Author URI: http://dustyf.com
+ * Version: 1.0.0
+ * License: GPLv2
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WDS_WP_Contributions' ) ) {
+
+	class WDS_WP_Contributions {
+
+		/**
+		 * Construct function to get things started.
+		 */
+		public function __construct() {
+			// Setup some base variables for the plugin
+			$this->basename       = plugin_basename( __FILE__ );
+			$this->directory_path = plugin_dir_path( __FILE__ );
+			$this->directory_url  = plugins_url( dirname( $this->basename ) );
+
+			// Include any required files
+			add_action( 'init', array( $this, 'includes' ) );
+
+			// Load Textdomain
+			load_plugin_textdomain( 'wp_contributions', false, dirname( $this->basename ) . '/languages' );
+
+			// Activation/Deactivation Hooks
+			register_activation_hook( __FILE__, array( $this, 'activate' ) );
+			register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+		}
+
+		/**
+		 * Include our plugin dependencies.
+		 */
+		public function includes() {
+			if ( $this->meets_requirements() ) {
+			}
+		}
+
+		/**
+		 * Activation hook for the plugin.
+		 */
+		public function activate() {
+			// If requirements are available, run our activation functions
+			if ( $this->meets_requirements() ) {
+			}
+		}
+
+		/**
+		 * Deactivation hook for the plugin.
+		 */
+		public function deactivate() {
+
+		}
+
+	}
+
+	$_GLOBALS['wp_contributions'] = new WDS_WP_Contributions();
+
+}
