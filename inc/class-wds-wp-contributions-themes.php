02ebe99d5585bfc8c3f7fbcdb46faafcbd241853
Dustin Filippini <dusty@dustyf.com>
2015-02-13 09:36:44 -0600

    Add include files to handle plugins and themes

---
+++ inc/class-wds-wp-contributions-themes.php
@@ -0,0 +1,16 @@
+<?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WDS_WP_Contributions_Plugins' ) ) {
+
+	class WDS_WP_Contributions_Plugins {
+
+		function __construct() {
+
+		}
+
+	}
+}
