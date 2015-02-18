2497e7675f0ba57ae5f259acc0f45ccaf06c03ce
Dustin Filippini <dusty@dustyf.com>
2015-02-18 10:35:54 -0600

    Add methods to pull data for a single plugin and all plugins for an author.

--- inc/class-wds-wp-contributions-plugins.php
+++ inc/class-wds-wp-contributions-plugins.php
@@ -20,7 +20,7 @@ if ( ! class_exists( 'WDS_WP_Contributions_Plugins' ) ) {
 		 *
 		 * @param string       $action Action the API should perform for it's query.
 		 * @param array|object $args   Optional. Arguments to serialize for the Plugin Info API.
-		 * @return object plugins_api response object on success, WP_Error on failure.
+		 * @return object $res response object on success, WP_Error on failure.
 		 */
 		function plugins_api( $action, $args = null ) {
 
@@ -63,6 +63,46 @@ if ( ! class_exists( 'WDS_WP_Contributions_Plugins' ) ) {
 			}
 
 			return $res;
+		}
+
+		/**
+		 * Get the plugin object from the WP.org API
+		 *
+		 * @param $plugin_slug The slug of the plugin hosted on WP.org.
+		 * @return object An object of the plugin data returned from the WP.org Plugin API.
+		 */
+		public function get_plugin( $plugin_slug ) {
+
+			if ( false === ( $plugin = get_transient( 'wp_contributions_plugin_' . $plugin_slug ) ) ) {
+				$args   = array(
+					'slug' => esc_attr( $plugin_slug ),
+				);
+				$plugin = $this->plugins_api( 'plugin_information', $args );
+				set_transient( 'wp_contributions_plugin_' . $plugin_slug, $plugin, 24 * HOUR_IN_SECONDS );
+			}
+
+			return $plugin;
+
+		}
+
+		/**
+		 * Get all plugins from WP.org by a certain Author.
+		 *
+		 * @param $author_name The username of the author you are querying for plugins.
+		 * @return object An object of the plugin data returned from the WP.org Plugin API.
+		 */
+		public function get_author_plugins( $author_name ) {
+
+			if ( false === ( $author = get_transient( 'wp_contributions_plugin_author_' . $author_name ) ) ) {
+				$args   = array(
+					'author' => esc_attr( $author_name ),
+				);
+				$author = $this->plugins_api( 'query_plugins', $args );
+				set_transient( 'wp_contributions_plugin_' . $author_name, $author, 24 * HOUR_IN_SECONDS );
+			}
+
+			return $author;
+
 		}
 
 	}
